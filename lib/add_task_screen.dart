866d4dde02d18e21a1ded69afd3b68c3427f276b
cyrus4u <cyrus4u@users.noreply.github.com>
2025-11-18 16:14:56 +0330

    updating Task model ,TimePicker

--- lib/add_task_screen.dart
+++ lib/add_task_screen.dart
@@ -3,6 +3,7 @@ import 'package:hive/hive.dart';
 import 'package:note_application_6/constants/custom_color.dart';
 import 'package:note_application_6/home_screen.dart';
 import 'package:note_application_6/task.dart';
+import 'package:time_pickerr/time_pickerr.dart';
 
 class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
@@ -15,6 +16,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   FocusNode negahban1 = FocusNode();
   FocusNode negahban2 = FocusNode();
   final TextEditingController controllerTaskTitle = TextEditingController();
+  final TextEditingController controllerTaskSubTitle = TextEditingController();
   final TextEditingController controllerTaskSubTitle = TextEditingController();
   @override
   void initState() {
@@ -35,105 +37,137 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 44),
-                child: Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: TextField(
-                    controller: controllerTaskTitle,
-                    focusNode: negahban1,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 15,
-                      ),
-                      labelText: 'عنوان تسک',
-                      labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: negahban1.hasFocus
-                            ? CustomColor.green
-                            : CustomColor.grey,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide: BorderSide(
-                          color: Color(0xffC5C5C5),
-                          width: 3.0,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44),
+                  child: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: TextField(
+                      controller: controllerTaskTitle,
+                      focusNode: negahban1,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 15,
                         ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide: BorderSide(
-                          width: 3,
-                          color: CustomColor.green,
+                        labelText: 'عنوان تسک',
+                        labelStyle: TextStyle(
+                          fontSize: 20,
+                          color: negahban1.hasFocus
+                              ? CustomColor.green
+                              : CustomColor.grey,
                         ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 44),
-                child: Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: TextField(
-                    controller: controllerTaskSubTitle,
-                    maxLines: 2,
-                    focusNode: negahban2,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 15,
-                      ),
-                      labelText: 'توضیحات تسک',
-                      labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: negahban2.hasFocus
-                            ? CustomColor.green
-                            : CustomColor.grey,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide: BorderSide(
-                          color: Color(0xffC5C5C5),
-                          width: 3.0,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide: BorderSide(
+                            color: Color(0xffC5C5C5),
+                            width: 3.0,
+                          ),
                         ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide: BorderSide(
-                          width: 3,
-                          color: CustomColor.green,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide: BorderSide(
+                            width: 3,
+                            color: CustomColor.green,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: CustomColor.green,
-                ),
-                onPressed: () {
-                  String taskTitle = controllerTaskTitle.text;
-                  String taskSubTitle = controllerTaskSubTitle.text;
-                  addTask(taskTitle, taskSubTitle);
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  'اضافه کردن تسک',
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                SizedBox(height: 50),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44),
+                  child: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: TextField(
+                      controller: controllerTaskSubTitle,
+                      maxLines: 2,
+                      focusNode: negahban2,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 15,
+                        ),
+                        labelText: 'توضیحات تسک',
+                        labelStyle: TextStyle(
+                          fontSize: 20,
+                          color: negahban2.hasFocus
+                              ? CustomColor.green
+                              : CustomColor.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide: BorderSide(
+                            color: Color(0xffC5C5C5),
+                            width: 3.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide: BorderSide(
+                            width: 3,
+                            color: CustomColor.green,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: CustomHourPicker(
+                    title: 'انتخاب زمان',
+                    titleStyle: TextStyle(
+                      color: CustomColor.green,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    negativeButtonStyle: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    positiveButtonStyle: TextStyle(
+                      color: CustomColor.green,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    negativeButtonText: 'حذف کن',
+                    positiveButtonText: 'انتخاب زمان',
+                    elevation: 2,
+                    onPositivePressed: (context, time) {
+                      print(time.hour);
+                      print(time.minute);
+                    },
+                    onNegativePressed: (context) {},
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: CustomColor.green,
+                  ),
+                  onPressed: () {
+                    String taskTitle = controllerTaskTitle.text;
+                    String taskSubTitle = controllerTaskSubTitle.text;
+
+                    addTask(taskTitle, taskSubTitle);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'اضافه کردن تسک',
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -141,7 +175,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   }
 
   addTask(String taskTitle, String taskSubTitle) {
-    var task = Task(title: taskTitle, subTitle: taskSubTitle);
+    var task = Task(title: taskTitle, subTitle: taskSubTitle,time: );
     box.add(task);
   }
 }
--- lib/task.dart
+++ lib/task.dart
@@ -2,12 +2,19 @@ import 'package:hive_flutter/hive_flutter.dart';
 part 'task.g.dart';
 
 @HiveType(typeId: 1)
-class Task extends HiveObject{
+class Task extends HiveObject {
   @HiveField(0)
   String title;
   @HiveField(1)
   String subTitle;
   @HiveField(2)
   bool isDone;
-  Task({required this.title, required this.subTitle, this.isDone = false});
+  @HiveField(3)
+  DateTime time;
+  Task({
+    required this.title,
+    required this.subTitle,
+    this.isDone = false,
+    required this.time,
+  });
 }
